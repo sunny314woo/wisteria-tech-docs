4dfa6e1f88433deedb425f617ea07e30acb0e868
sunny314woo <130764125+sunny314woo@users.noreply.github.com>
2026-09-17 21:47:54 +0800

    Fix broken recent article links in sidebar
    * Fix recent post sidebar links
    
    * Restore related post sorting

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -190,9 +190,10 @@ hexo.extend.helper.register('recent_posts_sidebar', function(limit) {
   let html = '<div class="links-of-blogroll animated">';
   html += '<div class="links-of-blogroll-title" data-i18n="recent_posts">近期文章</div>';
   html += '<ul class="links-of-blogroll-list">';
-  posts.forEach(function(post) {
+  posts.forEach(post => {
+    const title = escapeHtml(post.title);
     html += '<li class="links-of-blogroll-item">';
-    html += '<a href="' + hexo.config.root + post.path + '" title="' + post.title + '">' + post.title + '</a>';
+    html += '<a href="' + this.url_for(post.path) + '" title="' + title + '">' + title + '</a>';
     html += '</li>';
   });
   html += '</ul></div>';
